cb29587bac2340ce03e935d50f54f71092ca4a6f
allahgabo <suliman_allahgabo.com>
2022-12-11 11:33:13 +0300

    Changed the index

--- taskmate/views.py
+++ taskmate/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth.decorators import login_required
 
 
 def index(request):
-	return render(request,'main/index.html')
+	return render(request,'index.html')
 
 
 @login_required
